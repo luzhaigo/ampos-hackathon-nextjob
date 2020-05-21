5f7815c5d564e76f878ea4bd00f7698c98cd38aa
pohsun <po-hsun@ampostech.com>
2020-05-21 18:09:03 +0000

    chore: new build

---
+++ build/precache-manifest.54b482d246100fbc183564dd2e2b50ac.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f21658231dd473f153319ba974af00c2",
+    "url": "/ampos-hackathon-nextjob/index.html"
+  },
+  {
+    "revision": "87a4031c981b2119ba91",
+    "url": "/ampos-hackathon-nextjob/static/css/main.1b3040e5.chunk.css"
+  },
+  {
+    "revision": "caa89bdaf0dff9156e45",
+    "url": "/ampos-hackathon-nextjob/static/js/2.d6ebddcd.chunk.js"
+  },
+  {
+    "revision": "87a4031c981b2119ba91",
+    "url": "/ampos-hackathon-nextjob/static/js/main.c81affa0.chunk.js"
+  },
+  {
+    "revision": "5cdea2d2302695a68998",
+    "url": "/ampos-hackathon-nextjob/static/js/runtime~main.4680d650.js"
+  },
+  {
+    "revision": "b4c986564c33d68dadc5855dc76e21d6",
+    "url": "/ampos-hackathon-nextjob/static/media/Group_3.b4c98656.svg"
+  },
+  {
+    "revision": "77310c118bb1a47e5295d6429bbb99f6",
+    "url": "/ampos-hackathon-nextjob/static/media/Group_4.77310c11.svg"
+  },
+  {
+    "revision": "4b04c2ea86a5f6522b6510233ea579b1",
+    "url": "/ampos-hackathon-nextjob/static/media/Group_5.4b04c2ea.svg"
+  },
+  {
+    "revision": "792c1169b51a719b337c2e65759aa77e",
+    "url": "/ampos-hackathon-nextjob/static/media/Group_6.792c1169.png"
+  },
+  {
+    "revision": "2ee8d862017cda5b8e68e2188a1ea180",
+    "url": "/ampos-hackathon-nextjob/static/media/Shape.2ee8d862.svg"
+  },
+  {
+    "revision": "ef832a20f555123e2c996833074ff3e0",
+    "url": "/ampos-hackathon-nextjob/static/media/bumb.ef832a20.png"
+  },
+  {
+    "revision": "4a4e4d5573e5323e6f8c5c98c4d83f90",
+    "url": "/ampos-hackathon-nextjob/static/media/hint.4a4e4d55.svg"
+  },
+  {
+    "revision": "831f7a3421033a220030f9f5675d402b",
+    "url": "/ampos-hackathon-nextjob/static/media/nextjob.831f7a34.svg"
+  },
+  {
+    "revision": "aa2e81163414b63e443678fa47aad30a",
+    "url": "/ampos-hackathon-nextjob/static/media/truck.aa2e8116.png"
+  }
+]);
